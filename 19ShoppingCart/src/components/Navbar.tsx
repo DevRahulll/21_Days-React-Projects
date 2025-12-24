95de6d9634ae6585aae4147859fcf97cca1e42aa
Rahul <rahuldevan2019@gmail.com>
2025-12-24 17:28:32 +0530

    project 19 mini project shopping cart navbar complete

---
+++ 19ShoppingCart/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between p-2 mt-5">
+      <div className=" ml-10">
+        <h1 className="text-4xl font-bold truncate tracking-wider text-[#BB86FC]">
+          REACT REDUX SHOPPING CART
+        </h1>
+      </div>
+      <ul className="flex items-center gap-10 mr-14 text-2xl font-semibold">
+        <li className="list-none hover:scale-105 hover:text-[#BB86FC]">
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li className="list-none hover:scale-105 hover:text-[#BB86FC]">
+          <Link to={"/cart"}>Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
